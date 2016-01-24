981d7bf8e943892b644185336c0f3f7318b80a1a
Mark Olson <theothermarkolson@gmail.com>
2016-01-23 20:41:42 -0500

    Add UeberAuth Identity lib

--- config/config.exs
+++ config/config.exs
@@ -27,3 +27,8 @@ import_config "#{Mix.env}.exs"
 config :phoenix, :generators,
   migration: true,
   binary_id: true
+
+config :ueberauth, Ueberauth,
+  providers: [
+    identity: {Ueberauth.Strategy.Identity, []}
+  ]
--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :sports_team_go, SportsTeamGo.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "sports_team_go_dev",
   hostname: "localhost",
   pool_size: 10
