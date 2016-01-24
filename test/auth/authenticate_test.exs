a72352e979cd272cdf0bb3df99f5089d01daf273
Mark Olson <theothermarkolson@gmail.com>
2016-01-24 10:37:36 -0500

    Another test

--- test/auth/authenticate_test.exs
+++ test/auth/authenticate_test.exs
@@ -2,6 +2,8 @@ defmodule SportsTeamGo.AuthenticateTest do
   use SportsTeamGo.ModelCase
   alias SportsTeamGo.Authenticate
   alias SportsTeamGo.Repo
+  alias SportsTeamGo.User
+  alias SportsTeamGo.Authorization
   import Ecto.Query
 
   defp count_for(table), do: from(w in table) |> select([u], count(u.id)) |> Repo.all |> hd
@@ -30,16 +32,30 @@ defmodule SportsTeamGo.AuthenticateTest do
 
 
   test "creates a User/Auth if none exist", %{good_ident: g} do
-    assert 0 == count_for(SportsTeamGo.User), "User table is not blank"
+    assert 0 == count_for(User), "User table is not blank"
     assert 0 == count_for(SportsTeamGo.Authorization), "Auth table is not blank"
 
     Authenticate.fetch(g, Repo)
 
-    assert 1 == count_for(SportsTeamGo.User), "User table is blank"
+    assert 1 == count_for(User), "User table is blank"
     assert 1 == count_for(SportsTeamGo.Authorization), "Auth table is blank"
 
-    u = SportsTeamGo.User |> limit(1) |> Repo.all |> hd
-    assert u.email == "mowgli@jung.le"
-    assert u.name == "Mowgli"
+    user = User |> limit(1) |> Repo.all |> hd
+    assert user.email == "mowgli@jung.le"
+    assert user.name == "Mowgli"
+  end
+
+  test "creates an Auth if a user exists", %{good_ident: g} do
+    {:ok, user} = User.changeset(%User{}, %{name: "Mowgli", email: "mowgli@jung.le"}) |> Repo.insert
+
+    assert 1 == count_for(User), "User table is blank"
+    assert 0 == count_for(Authorization), "Auth table is not blank"
+
+    auth = Authenticate.fetch(g, Repo)
+
+    assert 1 == count_for(User), "User table has the wrong number of entries"
+    assert 1 == count_for(SportsTeamGo.Authorization), "Auth table is blank"
+
+    assert user.id == auth.user_id
   end
 end
