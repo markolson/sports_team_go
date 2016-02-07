92042133d8d46d4c082d135b281c96271eb6a984
Mark Olson <theothermarkolson@gmail.com>
2016-02-06 23:02:42 -0500

    List Teams

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,7 +4,7 @@ defmodule SportsTeamGo.PageController do
 
   def index(conn, params, user, claims) do
     if Guardian.Plug.current_token(conn) do
-      teams = SportsTeamGo.Repo.preload(user, :teams)
+      teams = SportsTeamGo.Repo.preload(user, :teams).teams
       render conn, "index.html", current_user: user, teams: teams
     else
       redirect(conn, to: auth_path(conn, :login, "identity"))
